433b2a571b37e17b3581b9b45ee73bf85d2763b6
Ray Holder <ray.holder+github@gmail.com>
2022-04-17 00:59:13 -0500

    Add replacement for Guava VisibleForTesting

---
+++ markoverator-core/src/main/java/com/github/megallo/markoverator/annotations/VisibleForTesting.java
@@ -0,0 +1,8 @@
+package com.github.megallo.markoverator.annotations;
+
+/**
+ * This marker interface is useful to signal that the intent of the visibility
+ * choice was made in order to ease testing.
+ */
+public @interface VisibleForTesting {
+}

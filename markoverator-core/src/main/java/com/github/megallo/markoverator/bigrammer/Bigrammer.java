291b28f6ef04b7c4dd0222cd4227c1a996710add
Ray Holder <ray.holder+github@gmail.com>
2022-04-17 01:35:14 -0500

    Remove deprecated Kryo methods from Bigrammer

--- markoverator-core/src/main/java/com/github/megallo/markoverator/bigrammer/Bigrammer.java
+++ markoverator-core/src/main/java/com/github/megallo/markoverator/bigrammer/Bigrammer.java
@@ -16,9 +16,6 @@
 
 package com.github.megallo.markoverator.bigrammer;
 
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.Input;
-import com.esotericsoftware.kryo.io.Output;
 import com.github.megallo.markoverator.annotations.VisibleForTesting;
 import com.github.megallo.markoverator.utils.Lists;
 import com.github.megallo.markoverator.utils.Pair;
@@ -26,8 +23,6 @@ import com.github.megallo.markoverator.utils.PartOfSpeechUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -358,52 +353,6 @@ public class Bigrammer {
         return Lists.reverse(generated);
     }
 
-    /**
-     * This method will be removed in a future release. Use BigramModelBuilder.buildModel() instead.
-     * Call me first!
-     * Initialize this object with a model based on the provided sentences.
-     * @param sentencesList a list of sentences: each sentence is pre-tokenized, usually into words
-     */
-    @Deprecated
-    public void buildModel(List<List<String>> sentencesList) {
-        HashMap<Pair, List<String>> forwardCache = new HashMap<>();
-        HashMap<Pair, List<String>> backwardCache = new HashMap<>();
-        List<String> fullWordList = new ArrayList<>();
-
-        // add sentence delimiters to get more natural sentence starts and ends
-        for (List<String> oneSentence : sentencesList) {
-            fullWordList.add(DELIM);
-            fullWordList.addAll(oneSentence);
-        }
-        fullWordList.add(DELIM); // don't forget the one at the end
-
-        // for each triplet
-        //   map of (<w1, w2> -> w3) = generates forward text
-        //   map of (<w2, w3> -> w1) = generates backward text
-
-        for (int i = 0; i < fullWordList.size() - 2; i++) {
-            String w1 = fullWordList.get(i);
-            String w2 = fullWordList.get(i+1);
-            String w3 = fullWordList.get(i+2);
-
-            Pair forwardPair = new Pair(w1, w2);
-            Pair backwardPair = new Pair(w2, w3);
-
-            if (!forwardCache.containsKey(forwardPair)) {
-                forwardCache.put(forwardPair, new ArrayList<String>());
-            }
-            forwardCache.get(forwardPair).add(w3);
-
-            if (!backwardCache.containsKey(backwardPair)) {
-                backwardCache.put(backwardPair, new ArrayList<String>());
-            }
-            backwardCache.get(backwardPair).add(w1);
-        }
-
-        model = new BigramModel(fullWordList, forwardCache, backwardCache);
-        calculateWordIndices();
-    }
-
     private void calculateWordIndices() {
         wordIndexMap = new HashMap<>();
 
@@ -499,40 +448,4 @@ public class Bigrammer {
 
         return true;
     }
-
-    /**
-     * This method will be removed in a future release. Use BigramModelBuilder.saveModel() instead.
-     *
-     * Save the model to a file.
-     * @param outputStream
-     */
-    @Deprecated
-    public void saveModel(OutputStream outputStream) {
-        if (model == null) {
-            throw new RuntimeException("Refusing to write empty model.");
-        }
-        Kryo kryo = new Kryo();
-        Output output = new Output(outputStream);
-        kryo.writeObject(output, model);
-        output.close();
-
-        loggie.info("Wrote model");
-    }
-
-    /**
-     * This method will be removed in a future release. Use BigramModelBuilder.loadModel() + setModel() instead.
-     *
-     * Load the model from a file.
-     */
-    @Deprecated
-    public void loadModel(InputStream inputStream) {
-        Kryo kryo = new Kryo();
-        Input input = new Input(inputStream);
-        model = kryo.readObject(input, BigramModel.class);
-        calculateWordIndices();
-        input.close();
-
-        loggie.info("Loaded model; found {} words", model.getFullWordList().size());
-    }
-
 }
